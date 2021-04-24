80cff81aba974d2d4438d185be60e56aee25d66c
Jordan Irwin <antumdeluge@gmail.com>
2021-04-23 19:34:23 -0700

    Show some stats when an item is placed in the examiner

--- formspec.lua
+++ formspec.lua
@@ -23,7 +23,7 @@ local function get_item_specs(item)
 			if not specs or specs == "" then
 				specs = k .. ": " .. v
 			else
-				specs = specs .. "," .. S("Damge to @1 type:", k) .. " " .. v
+				specs = specs .. "," .. S("Damge to @1 type: @2", k, v)
 			end
 		end
 	end
@@ -31,9 +31,9 @@ local function get_item_specs(item)
 	return specs
 end
 
-local function get_formspec(item)
+function equip_exam:get_formspec(item, empty)
 	local specs
-	if item then
+	if not empty then
 		specs = get_item_specs(core.registered_tools[item])
 	else
 		specs = "" -- empty
@@ -45,8 +45,6 @@ local function get_formspec(item)
 
 	local formspec = "formspec_version[4]"
 		.. "size[12,9]"
-		--.. "box[1,1;1,1;#555555]"
-		--.. "list[context;input;1,1;1,1;]"
 		.. "list[context;input;1,1;1,1;0]"
 		.. "button_exit[0.25,3.1;2.5,0.8;close;Close]"
 		.. "label[3,0.7;" .. S("Specs:") .. "]"
@@ -56,9 +54,13 @@ local function get_formspec(item)
 	return formspec
 end
 
-function equip_exam:show_formspec(player)
+function equip_exam:show_formspec(pos, player)
 	local playername = player:get_player_name()
 	if not playername or not player:is_player() then return end
 
-	core.show_formspec(playername, equip_exam.formspec_name, get_formspec())
+	local meta = core.get_meta(pos)
+	local inv = meta:get_inventory()
+	local contents = inv:get_list("input")[1]
+
+	core.show_formspec(playername, equip_exam.formspec_name, equip_exam:get_formspec(contents:get_name(), inv:is_empty("input")))
 end
--- node.lua
+++ node.lua
@@ -7,14 +7,28 @@ local node_def = {
 		"equip_exam_examiner_front.png",
 		"equip_exam_examiner.png",
 	},
+	groups = {oddly_breakable_by_hand=1,},
 	is_gound_content = false,
 	stack_max = 1,
-	on_rightclick = function(pos, node, player, itemstack, pointed_thing)
-		equip_exam:show_formspec(player)
-	end,
 	on_construct = function(pos)
-		local inv = core.get_meta(pos):get_inventory()
-		inv:set_size('input', 1)
+		local meta = core.get_meta(pos)
+		meta:set_string("formspec", equip_exam:get_formspec())
+		local inv = meta:get_inventory()
+		inv:set_size("input", 1)
+	end,
+	on_rightclick = function(pos, node, player, itemstack, pointed_thing)
+		equip_exam:show_formspec(pos, player)
+	end,
+	can_dig = function(pos, player)
+		local meta = core.get_meta(pos)
+		local inv = meta:get_inventory()
+		return inv:is_empty("input")
+	end,
+	on_metadata_inventory_put = function(pos, listname, index, stack, player)
+		equip_exam:show_formspec(pos, player)
+	end,
+	on_metadata_inventory_take = function(pos, listname, index, stack, player)
+		equip_exam:show_formspec(pos, player)
 	end,
 }
 
